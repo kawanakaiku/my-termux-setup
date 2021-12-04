0efc9db13021b35a73fe918db7cc03fd41138280
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2021-12-04 23:05:48 +0900

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -2,15 +2,19 @@
 
 termux-wake-lock
 
+log(){
+   printf "\033[0;34m$1\033[0m\n"
+}
+
 ##detect external sdcard path
-external="$((mount | awk '{print $3}' | grep -e "^/storage/" | grep -v -e "^/storage/emulated" | head -n1) || echo "/sdcard")"
+external="$( (mount | awk '{print $3}' | grep -e "^/storage/" | grep -v -e "^/storage/emulated" | head -n1) || echo "/sdcard")"
 
 ##Disable login message
-echo "Disabling login message"
+log "Disabling login message"
 echo -n | tee $HOME/../usr/etc/motd >/dev/null
 
 ##Enable fullscreen
-echo "Enabling fullscreen"
+log "Enabling fullscreen"
 string="fullscreen = true"
 termuxprop="$HOME/.termux/termux.properties"
 if ! grep -q "^${string}" "${termuxprop}"; then
@@ -19,7 +23,7 @@ fi
 unset string termuxprop
 
 ##Move Termux deb package to external
-echo "Moving Termux deb package to external"
+log "Moving Termux deb package to external"
 debcache="$HOME/../../cache/apt/archives"
 termuxtmp="$external/deb"
 rm -rf "$debcache"
@@ -28,7 +32,7 @@ ln -s "$termuxtmp" "$debcache"
 unset debcache termuxtmp
 
 ##Add SD variable
-echo "Addin SD variable"
+log "Addin SD variable"
 string="export SD=\"\$((mount | awk '{print \$3}' | grep -e \"^/storage/\" | grep -v -e \"^/storage/emulated\" | head -n1) || echo \"/sdcard\")\""
 bashrc="$HOME/.bashrc"
 if  [ -f "$bashrc" ] || ! grep -q "^${string}" "${bashrc}"; then
@@ -37,7 +41,7 @@ fi
 unset string bashrc
 
 ##Disable man-db
-echo "Disabling man-db"
+log "Disabling man-db"
 dpkgconf=" $PREFIX/etc/dpkg/dpkg.cfg.d/01_nodoc"
 strings=""
 mkdir -p "$(dirname $dpkgconf)"
@@ -57,8 +61,8 @@ fi
 unset strings dpkgconf
 
 ##Add ~/bin to PATH
-echo "Adding ~/bin to PATH"
-bin="$HOME/bin"
+log "Adding ~/bin to PATH"
+mkdir -p "$HOME/bin"
 bashrc="$HOME/.bashrc"
 string='export PATH="$PATH:$HOME/bin"'
 if [ -f "$bashrc" ] || ! grep -zoPq "^$bashrc" "$HOME/.bashrc"; then
@@ -67,29 +71,25 @@ fi
 unset bin bashrc string
 
 ##Install yt-dlp to ~/bin
-echo "Installing yt-dlp to ~/bin"
-curl --retry 5 -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o $HOME/bin/yt-dlp
-chmod a+rx $HOME/bin/yt-dlp
+log "Installing yt-dlp to ~/bin"
+curl --retry 5 -L "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp" -o "$HOME/bin/yt-dlp"
+chmod a+rx "$HOME/bin/yt-dlp"
 
 ##Install favorites
-echo "Installing favorites"
+log "Installing favorites"
 pkgs="ffmpeg bash-completion nano wget"
 pkg install -y $pkgs
 unset pkgs
 
 ##Put termux-url-opener
-echo "Putting termux-url-opener"
-cat <<"EOF" | tee $HOME/bin/termux-url-opener
+log "Putting termux-url-opener"
+cat <<"EOF" | tee "$HOME/bin/termux-url-opener"
 #!/data/data/com.termux/files/usr/bin/bash
-
 url="$1"
 SD="$((mount | awk '{print $3}' | grep -e "^/storage/" | grep -v -e "^/storage/emulated" | head -n1) || echo "/sdcard")"
 dir="$SD/Movies"
-
 yt-dlp -o "$dir/%(title)s.%(ext)s" "$url" || sleep 1m
 EOF
-
-
 
 termux-wake-unlock
 exit
